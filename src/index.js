67dee468290156823037d180f9f222d33b3b36d0
KonstHardy <konst.hardy@gmail.com>
2021-03-11 19:44:40 +0300

    [fix] project structure
    delete serviceWorker file;

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
 import App from "./js/App";
 import "./index.css";
 
@@ -10,5 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
