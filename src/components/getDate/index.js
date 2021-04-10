42d9596d7b98d479ba6d7bc91127ad481fe0c756
KonstHardy <konst.hardy@gmail.com>
2021-04-11 01:47:02 +0300

    [fix] getData
    [fix] getData for get the correct month;

--- src/components/getDate/index.js
+++ src/components/getDate/index.js
@@ -1,7 +1,7 @@
 function getDate() {
   let now = new Date();
   let year = now.getFullYear();
-  let month = now.getMonth();
+  let month = now.getMonth() + 1;
   let day = now.getDate();
   let hours = now.getHours();
   let minutes = now.getMinutes();
